85cd0a87761fbf2520d67a05afa6ef3d4eaf0003
AKASHDEEPMAHATA <akashdeepmahata@gmail.com>
2023-09-25 05:04:45 +0530

    files modified 1.0

--- qna/settings.py
+++ qna/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0^q4g%#h!7s625c)y=8@smxxf-c@ahd8r1@9#rd(08y4o0%!oo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
