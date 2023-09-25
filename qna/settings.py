2c1b2ce3e1a110b61be01d1a4f34de7d8908b6f9
AKASHDEEPMAHATA <akashdeepmahata@gmail.com>
2023-09-25 05:33:04 +0530

    files modified 1.1

--- qna/settings.py
+++ qna/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0^q4g%#h!7s625c)y=8@smxxf-c@ahd8r1@9#rd(08y4o0%!oo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
--- qna/wsgi.py
+++ qna/wsgi.py
@@ -15,4 +15,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qna.settings')
 
 application = get_wsgi_application()
 
-app = application
+# app = application 
